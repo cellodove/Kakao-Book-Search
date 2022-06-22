c8e217b72dcfd63425a1c9ba19cab18b9e44b148
jusung.kim <wntjd200@naver.com>
2022-06-22 17:58:18 +0900

    search word change color

--- presentation/src/main/java/com/cellodove/presentation/ui/search/BookViewHolder.kt
+++ presentation/src/main/java/com/cellodove/presentation/ui/search/BookViewHolder.kt
@@ -2,26 +2,31 @@ package com.cellodove.presentation.ui.search
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.cellodove.book.R
 import com.cellodove.book.databinding.BookListItemBinding
 import com.cellodove.domain.model.response.Documents
 
 class BookViewHolder(private val binding:BookListItemBinding) :  RecyclerView.ViewHolder(binding.root){
 
     fun bind(documents : Documents, searchWord : String){
-        showData(documents)
+        showData(documents,searchWord)
     }
 
-    private fun showData(documents : Documents){
+    private fun showData(documents : Documents,searchWord : String){
         binding.apply {
             Glide.with(binding.root)
                 .load(documents.thumbnail)
                 .override(100,100)
                 .into(binding.ImageThumbnail)
-            bookName.text = documents.title
+            //bookName.text = documents.title
             bookPrice.text = documents.price.toString()
+
+            val text = documents.title
+            val htmlText = text.replace(searchWord, "<font color='#0be3d8'>$searchWord</font>")
+            bookName.text = HtmlCompat.fromHtml(htmlText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
         }
     }
 
